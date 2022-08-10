bb672c95f375b1218c3bbe3d2e32d3cde708a0c3
Sergey <hojalata@mail.ru>
2022-08-10 14:28:28 +0300

    Выполнил задачу №13

---
+++ Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberArray = Convert.ToString(number);
+
+if(numberArray.Length > 2)
+{
+    Console.WriteLine($"Третья цифра {numberArray[2]}");
+}
+else
+{
+   Console.WriteLine("Третьей цифры нет!"); 
+}
