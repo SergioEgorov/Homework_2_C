d1d777bb10106ce32d4d159807c58463ce4ef9cb
Sergey <hojalata@mail.ru>
2022-08-10 17:47:18 +0300

    Выполнил задачу №15

---
+++ Task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите цифру дня недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void dayOfWeek(int dayNumber)
+{
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine("Этот день недели выходной");
+    }
+    else if (dayNumber <1 || dayNumber > 7)
+    {
+        Console.WriteLine("Такого дня недели не существует!");
+    }
+    else 
+    {
+        Console.WriteLine("Этот день недели невыходной");
+    }
+
+
+}
+dayOfWeek(dayNumber);
